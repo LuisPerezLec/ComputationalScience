a2c8c337d3d802719af4253fa17b69fe081ae998
LuisPerezLec <93443241+LuisPerezLec@users.noreply.github.com>
2023-12-21 18:57:22 +0000

    Python's course

---
+++ Tutorial01.py
@@ -0,0 +1,23 @@
+'''
+poundsPerKg = 2.20462
+weight = input('Weight (kg): ')
+weightOnPounds = float(weight) * poundsPerKg
+print(weightOnPounds)
+
+string = 'pyton\'s'
+
+first = 'Pablo'
+last = 'Rodriguez'
+
+msg = f'{first} [{last}] is a coder'
+print(msg)
+'''
+# len() <- General purpose function
+myName = "Luis Ernesto Pérez Lechuga"
+print(len(myName))
+print(myName.swapcase())
+print(myName.find('E'))
+print(myName.replace('Luis', 'Luisille'))
+print('Lechuga' in myName)
+
+#https://youtu.be/_uQrJ0TkZlc?t=3017
